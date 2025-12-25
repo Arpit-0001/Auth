2ab5e76c847b11a92f5fff0e611ac0820928d023
Arpit-0001 <mysteriousgamer0001@gmail.com>
2025-12-25 14:30:20 +0530

    Update Program.fs

--- AuthServer/Program.fs
+++ AuthServer/Program.fs
@@ -1,6 +1,5 @@
 namespace AuthServer
 
-open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 
@@ -9,12 +8,9 @@ module Program =
     [<EntryPoint>]
     let main args =
         let builder = WebApplication.CreateBuilder(args)
-
         let app = builder.Build()
 
-        app.MapGet("/", fun () ->
-            Results.Ok("AuthServer is running")
-        ) |> ignore
+        app.MapGet("/", fun () -> "AuthServer running") |> ignore
 
         app.Run()
         0
